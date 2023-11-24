f0242b5d8f5a168cd2d212cf2c8b5f39783b0f83
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-24 00:20:58 -0300

    teste

--- src/ros2_ws/src/jose_entregas/jose_entregas/teste.py
+++ src/ros2_ws/src/jose_entregas/jose_entregas/teste.py
@@ -10,7 +10,7 @@ def msg(content):
         model="gpt-3.5-turbo",  # ou outro modelo
     messages=[
         {"role": "system", "content": "Considere que você é um técnico de almoxarifado especializado em peças mecânicas e funcionamento fabril"},
-        {"role": "user", "content": f"Preciso que para cada problema relacionado ao mundo fabril você me forneça uma solução de que peça estou precisando para solucionar de forma breve, curta e em todos as suas respostas a palavra 'essa peça' deve estar antes da peça que você me recomendar. {content}"}
+        {"role": "user", "content": f"Preciso que me retorne a posição digitada a frente entre '()': {content}"}
     ])
 
     return completion.choices[0].message
