80cd6db07741501c474c87ac83005c981494239e
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-12-04 22:12:51 -0300

    UPDATE-server_bot

--- src/api/server.js
+++ src/api/server.js
@@ -1,21 +1,46 @@
 const net = require('net')
-
+const crypto = require('crypto');
 const { Client, LocalAuth  } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(
         {
-            clientId:"jv"
+            clientId:"pedro"
         }
     )
 });
- 
 
-var intent_dict = {
-    "\\b^([Qq]uero|[Pp]reciso|[Ee]stou|[Oo]nde encontro|[Pp]rocuro)\\s+[a-zA-Z\\s]+\\s(.+)$": "control",
-    "\\b[Vv][áa](?:\\spara)?\\s?[oa]?\\s(.+)": "go_to",
-    "\\b[Mm]e\\sleve até\\s?[oa]?\\s(.+)": "go_to"
-};
+
+var users = {
+    
+}
+
+
+function namefind(numero){
+    if (users[numero]){
+        console.log("User cadastrado ----> ", users[numero])
+        return true
+    }
+
+}
+
+var cadastrado ={
+
+}
+
+
+function validacao(numero){
+    if (cadastrado[numero]){
+        console.log("User cadastrado ----> ", users[numero])
+        return false
+    }
+    else{
+        cadastrado[numero] = "Em cadastro"
+        return true
+    }
+
+}
+
 
 
 client.on('qr', (qr) => {
@@ -28,19 +53,40 @@ client.on('ready', () => {
 
 client.initialize();
 
-client.on('message_create', async msg => {
+client.on('message', async msg => {
     if(msg.hasMedia) {
         const media = await msg.downloadMedia();
         
     }
     else{
         //msg.reply('Oi eu sou o jose entregas!');
-        if (msg.fromMe)
-        {try {
-            console.log(intent_dict[msg.body])
-        } catch (error) {
-            console.log("Ação não encontrada")
-        }}
+        if (msg.body){
+            if (namefind(msg.from)){
+                if (cadastrado[msg.from] == "Em cadastro" || cadastrado[msg.from] == "Pedido finalizado"){
+                    msg.reply(`Olá ${users[msg.from]}, em que posso ajudar?`);
+                    cadastrado[msg.from] = "Em uso"
+                }else{
+                    
+                }
+                
+            }else{
+                if (validacao(msg.from)){
+                    console.log("Me envie seu nome completo:")
+                    client.sendMessage(msg.from,"Me envie seu nome completo:");
+                }
+                else{
+                    users[msg.from] = msg.body
+                    client.sendMessage(msg.from,'Cadastro realizado com sucesso! Gostaria de realizar um pedido?');
+                   
+
+                }
+            }
+                
+           
+            console.log(msg.body);
+            //console.log(msg)
+            
+        }
     }
 
 });
