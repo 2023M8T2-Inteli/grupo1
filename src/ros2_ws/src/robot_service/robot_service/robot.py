b8fa7013731e1b93245d5309591e09255cc6d532
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-12-13 17:45:53 -0300

    UPSATE_robot_merge

--- src/ros2_ws/src/robot_service/robot_service/robot.py
+++ src/ros2_ws/src/robot_service/robot_service/robot.py
@@ -1,15 +1,16 @@
-import rclpy
-from nav2_simple_commander.robot_navigator import BasicNavigator
-from geometry_msgs.msg import PoseStamped
-from tf_transformations import quaternion_from_euler
-from std_msgs.msg import String
+#! /usr/bin/env python3
 import re
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from nav2_simple_commander.robot_navigator import TaskResult
 
+from .navigation_controller import Navigation
 
-class Robot():
+class Robot(Node):
     def __init__(self):
         super().__init__('robot_node')
-        self._publisher = self.create_publisher(String, 'api_topic', 10)
+        self._publisher = self.create_publisher(String, 'whatsApp_topic', 10)
         self._subscriber = self.create_subscription(
             String,
             'chatbot_topic',
@@ -17,64 +18,52 @@ class Robot():
             10)
         self._logger = self.get_logger()
         self._msg = String()
-        self.nav = BasicNavigator()
-        q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, 0.0)
-        initial_pose = PoseStamped()
-        initial_pose.header.frame_id = 'map'
-        initial_pose.header.stamp = self.nav.get_clock().now().to_msg()
-        initial_pose.pose.position.x = 0.0
-        initial_pose.pose.position.y = 0.0
-        initial_pose.pose.position.z = 0.0
-        initial_pose.pose.orientation.x = q_x
-        initial_pose.pose.orientation.y = q_y
-        initial_pose.pose.orientation.z = q_z
-        initial_pose.pose.orientation.w = q_w
+        self._nav = Navigation()
 
-        self.nav.setInitialPose(initial_pose)
-        self.nav.waitUntilNav2Active()
-
-
-    def go_to(self):
-        goal_pose3 = self._create_pose_stamped(self.nav, 0.0, 0.0, 0.00)
-        goal_pose1 = self._create_pose_stamped(self.nav, 2.5, 1.0, 1.57)
-        goal_pose2 = self._create_pose_stamped(self.nav, 0.0, 1.0, 1.57)
-        waypoints = [goal_pose1, goal_pose2, goal_pose3]
-        self.nav.followWaypoints(waypoints)
-        while not self.nav.isTaskComplete():
-            print(self.nav.getFeedback())
-
-
-
-    def _create_pose_stamped(navigator, pos_x, pos_y, rot_z):
-        q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, rot_z)
-        pose = PoseStamped()
-        pose.header.frame_id = 'map'
-        pose.header.stamp = navigator.get_clock().now().to_msg()
-        pose.pose.position.x = pos_x
-        pose.pose.position.y = pos_y
-        pose.pose.position.z = pos_x
-        pose.pose.orientation.x = q_x
-        pose.pose.orientation.y = q_y
-        pose.pose.orientation.z = q_z
-        pose.pose.orientation.w = q_w
-        return pose
-
-    def listener_callback(msg,self):
+    def listener_callback(self, msg):
+        """
+        This function purpose is to receive the data from the chatbot topic
+        """
         self._logger.info(f'Robot received: {msg.data}')
-        self.go_to()
+        self._logger.warning('Passing data to navigation controller')
+        self._msg = msg.data
+        return self._msg
 
 
+    def get_input_position(self):
+        """
+        This function purpose is to get the position from the chatbot
+        using a regex, then returning it as a list of integers
+        """
+        input_text = self.listener_callback()
+        match = re.findall(r'\b\d+\b', input_text)
+        position = [int(match) for i in match[-2:]]
+        return position
+
+    def move_towards_required_position(self):
+        """
+        This function purpose is to create a pose and move the robot
+        """
+        position = self.get_input_position()
+        self._nav.create_pose(position[0], position[1], 0.0)
+
+    def cheking_status(self):
+        """
+        Checks the status of a task after moving towards the required position.
+        Returns:
+        bool: True if the task is completed successfully, False otherwise.
+        """
+        self.move_towards_required_position()
+        task_status = self._nav.robot_navigation_status()
+
+        return task_status if task_status == TaskResult.SUCCEEDED else False
 
 def main():
     rclpy.init()
-    node = Robot()
-    rclpy.spin(node)
-    node.destroy_node()
+    robot = Robot()
+    rclpy.spin(robot)
+    robot.destroy_node()
     rclpy.shutdown()
-
-
-
-
 
 if __name__ == '__main__':
     main()
